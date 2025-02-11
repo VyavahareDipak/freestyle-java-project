f3cc01a6df2c88a0a483e52c0b5865cd4e782b6d
DipakVyavahre <vyavahared7@gmail.com>
2025-02-11 15:11:45 +0530

    time stamp code added

--- main.java
+++ main.java
@@ -1,6 +1,16 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// Get the current date and time
+LocalDateTime now = LocalDateTime.now();
+
+// Define the format
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+// Format the current date and time
+String formattedNow = now.format(formatter);
+
+// Print the formatted date and time
+System.out.println("Current Timestamp:" + formattedNow);
 	}
 }
