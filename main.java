4123a7ead59c19dcaccbdaa8155ff0a80acbd596
DipakVyavahre <vyavahared7@gmail.com>
2025-02-11 15:24:51 +0530

    time stamp code added to get current time and date

--- main.java
+++ main.java
@@ -1,17 +1,11 @@
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;
 public class Main
 {
 	public static void main(String[] args) {
-		// Get the current date and time
-LocalDateTime now = LocalDateTime.now();
-
-// Define the format
-DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-// Format the current date and time
-String formattedNow = now.format(formatter);
-
-// Print the formatted date and time
-System.out.println("Current Timestamp:" + formattedNow);
+		  DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();  
+        System.out.println("Current Date and Time: " + dtf.format(now));  
 	}
 }
 
